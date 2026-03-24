92df29b7c3553275ec267ad4ae6ee721d1d4b379
lukesh <adelukesh@gmail.com>
2026-03-24 11:16:18 +0530

    adding dialogs or alert or JSalerts

--- tests/dialogs.spec.ts
+++ tests/dialogs.spec.ts
@@ -12,18 +12,69 @@ test("Simple Dialog", async ({ page }) => {
     await page.goto("https://testautomationpractice.blogspot.com/");
 
 
+    // Register a dialog handler
     page.on("dialog", (dialog) => {
-        console.log("Dialog type is", dialog.type()); // returns type of dialog
+        console.log("Dialog type is:", dialog.type()); // returns type of dialog
         expect(dialog.type()).toContain('alert');
         console.log("Dialog text:", dialog.message()); // returns message from dialog 
         expect(dialog.message()).toContain("I am an alert box!");
         dialog.accept();
     });
-    
-
     await page.locator("#alertBtn").click(); // opens dialog
+    await page.waitForTimeout(5000);
+});
+
+
+
+
+test("confirmation Dialog", async ({ page }) => {
+
+    await page.goto("https://testautomationpractice.blogspot.com/");
+
+
+    // Register a dialog handler
+    page.on("dialog", (dialog) => {
+        console.log("Dialog type is:", dialog.type()); // returns type of dialog
+        expect(dialog.type()).toContain('confirm');
+        console.log("Dialog text:", dialog.message()); // returns message from dialog 
+        expect(dialog.message()).toContain("Press a button!");
+        dialog.accept(); // close dialog by accepting
+        // dialog.dismiss(); // close dialog by dimissing
+    });
+    await page.locator("#confirmBtn").click(); // opens conformation dialog
+
+    const text: string = await page.locator("#demo").innerText();
+    console.log("Output text:", text);
+    // await expect(page.locator("#demo")).toHaveText("You pressed Cancel!");
+    await expect(page.locator("#demo")).toHaveText("You pressed OK!");
 
     await page.waitForTimeout(5000);
+});
 
 
+
+
+test.only("Prompt Dialog", async ({ page }) => {
+
+    await page.goto("https://testautomationpractice.blogspot.com/");
+
+
+    // Register a dialog handler
+    page.on("dialog", (dialog) => {
+        console.log("Dialog type is:", dialog.type()); // returns type of dialog
+        expect(dialog.type()).toContain('prompt');
+        console.log("Dialog text:", dialog.message()); // returns message from dialog 
+        expect(dialog.message()).toContain("Please enter your name:");
+
+        expect(dialog.defaultValue()).toContain("Harry Potter"); // checks default value of the dialog
+
+        dialog.accept('Lukesh'); // close dialog by accepting
+        // dialog.dismiss(); // close dialog by dimissing
+    });
+    await page.locator("#promptBtn").click(); // opens prompt dialog
+
+    const text: string = await page.locator("#demo").innerText();
+    console.log("Output text:", text);
+    await expect(page.locator("#demo")).toHaveText("Hello Lukesh! How are you today?");
+    await page.waitForTimeout(5000);
 });
