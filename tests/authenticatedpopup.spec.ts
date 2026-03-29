2c6587748c485e064c6530c36a7e353e125310b1
lukesh <adelukesh@gmail.com>
2026-03-30 02:58:50 +0530

    update

--- tests/authenticatedpopup.spec.ts
+++ tests/authenticatedpopup.spec.ts
@@ -5,13 +5,13 @@ test("authenticated popups", async ({browser}) => {
     const context = await browser.newContext({httpCredentials: {username:'admin', password:'admin'}});
 
     const page =await context.newPage();
-
+    
+    // URL
     // https://the-internet.herokuapp.com/basic_auth
+
+    // Syntax
     // https://username:password@the-internet.herokuapp.com/basic_auth
 
-    
-    
-    
     
     /* // Approach 1: directly pass login along with the url
     // await page.goto("https://the-internet.herokuapp.com/basic_auth");
@@ -23,7 +23,6 @@ test("authenticated popups", async ({browser}) => {
 
     await page.waitForTimeout(5000); */
  
-
 
      // Approach 2: Pass the login along with browser context  (Most peferable approach in playwright)
     await page.goto("https://the-internet.herokuapp.com/basic_auth");
