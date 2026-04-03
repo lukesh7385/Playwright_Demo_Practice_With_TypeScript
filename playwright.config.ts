be0c990a7dd44b9e3ba88b64b4a598a6d765ca61
lukesh <adelukesh@gmail.com>
2026-04-04 00:36:18 +0530

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,9 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -18,7 +18,7 @@ export default defineConfig({
   // timeout: 60000,
 
   // To apply a longer wait for all expect conditions (default is 5000 ms / 5 secs) - by lukesh
-  expect: {timeout: 10000},
+  // expect: {timeout: 10000},
 
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -35,6 +35,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
+    // viewport: {width: 1280, height: 720},
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     // testIdAttribute: 'data-pw'// configured data-testid
